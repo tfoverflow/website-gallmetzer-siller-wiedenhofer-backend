bd4302eb7e0b9d3f946335c35ac8e54917529728
felix <felix.gallmetzer@gmail.com>
2023-12-18 08:31:20 +0100

    now using pool in login

--- file/file.model.js
+++ file/file.model.js
@@ -1,11 +1,5 @@
-const { v4: uuid } = require("uuid");
 const mysql = require("mysql");
-const connectionProperties = {
-  host: "ulsq0qqx999wqz84.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
-  user: "g5ec4ljgfr0kwo61",
-  password: "mae6mu11wtk59d7k",
-  database: "de7rtnl9b2hldxvv",
-};
+
 
 const pool = mysql.createPool({
   connectionLimit: 5,
@@ -15,40 +9,7 @@ const pool = mysql.createPool({
   database: "de7rtnl9b2hldxvv",
 });
 
-class Database {
-  constructor(connectionProperties) {
-    this.connection = mysql.createConnection(connectionProperties);
-  }
-  query(sql, params) {
-    return new Promise((resolve, reject) => {
-      this.connection.query(sql, params, (error, result) => {
-        if (error) {
-          console.log("encountered error %o", error);
-          reject(error);
-        }
-        resolve(result);
-      });
-    });
-  }
-  queryClose(sql, params) {
-    console.log("starting query");
-    const ret = this.query(sql, params);
-    console.log("finished query\nret: %o\nnow closing", ret);
-    this.close();
-    console.log("closed");
-    return ret;
-  }
-  close() {
-    return new Promise((resolve, reject) => {
-      this.connection.end((error) => {
-        if (error) {
-          reject(error);
-        }
-        resolve();
-      });
-    });
-  }
-}
+
 
 let data = [];
 
@@ -144,24 +105,31 @@ async function getUser(username, password) {
   if (!username || !password) {
     return Promise.reject("User not set");
   } else {
-    try {
-      const database = new Database(connectionProperties);
-      const sql = `
+    const passwordHash = crypto
+      .createHash("sha256")
+      .update(password)
+      .digest("hex");
+    const sql = `
         SELECT uid, uusername, ufirstname, ulastname, upasswordhash
         FROM users
-        WHERE uusername = ? AND upasswordhash = ?;
+        WHERE uusername = ? AND upasswordhash = ?
         `;
-      const passwordHash = crypto
-        .createHash("sha256")
-        .update(password)
-        .digest("hex");
-      const result = await database.queryClose(sql, [username, passwordHash]);
-      return !result || result.length === 0
-        ? Promise.reject("User not found")
-        : Promise.resolve(result[0]);
-    } catch (error) {
-      return Promise.reject("Database error");
-    }
+
+    return new Promise((resolve, reject) => {
+      pool.query(sql, [username, passwordHash], (error, result) => {
+        if (error) {
+          console.log("Error in query:", error);
+          reject("Database error");
+          return;
+        }
+
+        if (!result || result.length === 0) {
+          reject("User not found");
+        } else {
+          resolve(result[0]);
+        }
+      });
+    });
   }
 }
 module.exports = { getAll, remove, get, save, getUser };
